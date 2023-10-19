9d4339bbbbd312089c55c42dc8721a7059f879e0
NikrOKvillon <kiradmitrev19861101ar@gmail.com>
2023-10-19 22:59:44 +0300

    chore: Updated dependencies

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект ФИО";
+  const greeting = "Кононов К.Д. (02.03.03 - АИСа - о20)";
 
-  console.log('This is App ');
+  /*console.log('This is App ');*/
   return (
     <div className="App">
      <header className="App-header">
